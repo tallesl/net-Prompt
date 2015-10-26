ec62db629cad318272392e43a2d2e394bb903896
Talles L <talleslasmar@gmail.com>
2015-10-26 15:15:21 -0200

    added Title method

---
+++ Library/Public/Title.cs
@@ -0,0 +1,16 @@
+﻿namespace PromptUtil
+{
+    using System;
+
+    public static partial class Prompt
+    {
+        /// <summary>
+        /// Sets the console title.
+        /// </summary>
+        /// <param name="title">Title to set</param>
+        public static void Title(string title)
+        {
+            Console.Title = title;
+        }
+    }
+}
