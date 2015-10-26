8152a01b3d7bba033a3f869da1702eb1f4f6b5f9
Talles L <talleslasmar@gmail.com>
2015-10-26 15:18:52 -0200

    updated usage

--- Example/Program.cs
+++ Example/Program.cs
@@ -6,17 +6,21 @@
     {
         static void Main(string[] args)
         {
+            Prompt.Title("Silly example");
+
             var text = Prompt.Ask("Give me some string");
-            Prompt.Say("So your string is \"" + text + "\" eh?");
+            Prompt.Say("So your string is \"{0}\" eh?", text);
 
             var boolean = Prompt.AskYesOrNo("Are you a Yes or a No person?");
-            Prompt.Say("So you're a " + (boolean ? "Yes" : "No") + " person eh?");
+            Prompt.Say("So you're a \"{0}\" person eh?", boolean ? "Yes" : "No");
 
             var integer = Prompt.Ask<int>("Give me an integer");
-            Prompt.Say("So your integer is " + integer + " eh?");
+            Prompt.Say("So your integer is \"{0}\" eh?", integer);
 
             var uuid = Prompt.Ask<Guid>("Give me a UUID", Guid.NewGuid());
-            Prompt.Say("So your UUID is " + uuid + " eh?");
+            Prompt.Say("So your UUID is \"{0}\" eh?", uuid);
+
+            Prompt.Say("Okay, thanks, bye!");
         }
     }
 }
