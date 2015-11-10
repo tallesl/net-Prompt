83923b798990007daf9cc8346cca981001822a50
Talles L <talleslasmar@gmail.com>
2015-11-10 11:09:10 -0200

    PressAnyKeyToContinue method

---
+++ Library/Public/PressAnyKeyToContinue.cs
@@ -0,0 +1,16 @@
+﻿namespace PromptUtil
+{
+    using System;
+
+    public static partial class Prompt
+    {
+        /// <summary>
+        /// Says a "press any key to continue" message and waits for a key to proceed execution.
+        /// </summary>
+        public static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("(press any key to continue)");
+            Console.ReadKey();
+        }
+    }
+}
