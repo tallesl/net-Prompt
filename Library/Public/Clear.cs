a468e2f87fcaeafd454de583a0cf0f69549c23d2
Talles L <talleslasmar@gmail.com>
2015-11-10 11:07:33 -0200

    Clear method

---
+++ Library/Public/Clear.cs
@@ -0,0 +1,15 @@
+﻿namespace PromptUtil
+{
+    using System;
+
+    public static partial class Prompt
+    {
+        /// <summary>
+        /// Cleras the console title.
+        /// </summary>
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
